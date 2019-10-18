ce26b303b742f70a391f0eee30495d881fc0eb23
Renan <renan_ota@hotmail.com>
2019-10-18 01:26:14 -0300

    Adicionada mensagem de /start

--- src/bot.php
+++ src/bot.php
@@ -24,6 +24,13 @@ if (strpos($text, '/getlog') !== false) {
     die;
 }
 
+// Verificar se o comando não é o /start
+if (strpos($text, '/start') !== false) {
+    $response = "Seja bem vindo ao ReminderMessagesBot.\n\nPara adicionar um lembrete, digite naturalmente como nos exemplos abaixo:\n'Entregar trabalho hoje as 19:00'\n'Entregar trabalho, amanhã, as 08:00'\n'Entregar trabalho, dia 12/10/2019 07:30'\n\nEu consigo identificar palavras chaves como 'hoje' e 'amanhã' para salvar seus lembretes, ou você pode digitar a data completa.\nTodo lembrete precisa ter um horário, informe sempre o horário no formato 'hh:mm'.\n\nQuando chegar o dia e a hora marcada, eu te mando uma mensagem ;)";
+    sendMessage($chatId, $response);
+    die;
+}
+
 // Reconhecer os elementos chaves do lembrete
 $data       = recognizer($text);
 $response   = "";
