9437bf88a0a19db56f2be1b546b6efb27233dfb0
Renan Santana <renan_ota@hotmail.com>
2020-09-23 17:36:35 -0300

    Início da "conteinização"

--- src/connect.php
+++ src/connect.php
@@ -7,8 +7,9 @@ $password   = $config['password'];
 $db         = $config['db'];
 
 try {
-    $conn = new PDO("mysql:host=$server;dbname=$db", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // $conn = new PDO("mysql:host=$server;dbname=$db", $username, $password);
+    // $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn = pg_connect("postgres://$username:$password@$server/$db");
 } catch(PDOException $e) {
     echo $e;
 }
